cddc83d2a93ce33f3aca4391a553245e1f0e15e2
Katchi Dioba Chris Uriel Soro <thedyy@Katchis-MacBook-Air.local>
2023-09-17 13:42:02 +0000

    first commit

---
+++ Resigate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Resigate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resigate.settings')
+
+application = get_asgi_application()
